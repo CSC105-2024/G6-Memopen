36427c121798e61bc2486384e9b8a22dbbec2e0e
NichapaJongKmutt <nichapa.jong@mail.kmutt.ac.th>
2025-04-25 10:32:57 +0700

    pop up fix

--- Memopen/src/components/editor_popup/PostSucessPopUp.jsx
+++ Memopen/src/components/editor_popup/PostSucessPopUp.jsx
@@ -5,12 +5,12 @@ function PostSucessPopUp({ setSavePop , saveCanvas}) {
   return (
     <div>
       <div className="fixed inset-0  bg-black/40  bg-opacity-50 z-40"></div>        
-        <div className="SavePopUp fixed inset-0 justify-center items-center  flex flex-col  z-100  ">
+        <div className="SavePopUp mx-10 fixed inset-0 justify-center items-center  flex flex-col  z-100  ">
             <div className=" rounded-t-3xl relative flex flex-col px-20 py-13 bg-[#00D639] w-full max-w-sm">
                     <button className="absolute left-[74%] top-[10px] " onClick={()=> setSavePop(false)}><img src="\assets\PostSucessPopup\EditPopUpClose.svg"/></button>
                     <img className="justify-center items-center" src="\assets\PostSucessPopup\sucessPic.svg"/>
             </div>
-                <div className=" rounded-b-3xl savePoPButtom bg-white flex flex-col gap-9 px-8 py-12 w-full max-w-sm">
+                <div className=" rounded-b-3xl savePoPButtom bg-white flex flex-col gap-9 px-8 py-10 w-full max-w-sm">
                     <div className="editPopUpTextField">
                         <h3 className="font-bold text-3xl text-center">DONE!</h3>
                         <p className="text-1xl text-center mt-3" >You successfully saved your work</p>
