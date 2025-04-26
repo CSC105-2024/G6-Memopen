f7909bd3dc6547f2e409c3a8b28af0faca2a9ca9
NichapaJongKmutt <nichapa.jong@mail.kmutt.ac.th>
2025-04-26 12:01:37 +0700

    wtf

--- Memopen/src/components/editor_popup/PostSucessPopUp.jsx
+++ Memopen/src/components/editor_popup/PostSucessPopUp.jsx
@@ -7,8 +7,8 @@ function PostSucessPopUp({ setSavePop , saveCanvas}) {
       <div className="fixed inset-0  bg-black/40  bg-opacity-50 z-40"></div>        
         <div className="SavePopUp mx-10 fixed inset-0 justify-center items-center  flex flex-col  z-100  ">
             <div className=" rounded-t-3xl relative flex flex-col px-20 py-13 bg-[#00D639] w-full max-w-sm">
-                    <button className="absolute left-[74%] top-[10px] " onClick={()=> setSavePop(false)}><img src="\assets\PostSucessPopup\EditPopUpClose.svg"/></button>
-                    <img className="justify-center items-center" src="\assets\PostSucessPopup\sucessPic.svg"/>
+                    <button className="absolute left-[74%] top-[10px] " onClick={()=> setSavePop(false)}><img src="/assets/PostSucessPopup/EditPopUpClose.svg"/></button>
+                    <img className="justify-center items-center" src="/assets/PostSucessPopup/sucessPic.svg"/>
             </div>
                 <div className=" rounded-b-3xl savePoPButtom bg-white flex flex-col gap-9 px-8 py-10 w-full max-w-sm">
                     <div className="editPopUpTextField">
