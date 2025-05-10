13305b036a20d67a73fe805afab4d8eafa539bde
NichapaJongKmutt <nichapa.jong@mail.kmutt.ac.th>
2025-05-10 14:51:13 +0700

    br

--- backend-Memopen/src/routes/auth.routes.ts
+++ backend-Memopen/src/routes/auth.routes.ts
@@ -4,5 +4,5 @@ import { getAllUsers, login, signup } from "../controllers/auth.controller.ts";
 const authRouter = new Hono();
 authRouter.post('/register',signup);
 authRouter.post('/login',login);
-authRouter.get('/',getAllUsers)
+authRouter.get('/',getAllUsers)  //เดวลบ เอาไว้ดูเฉญๆ
 export default authRouter;
