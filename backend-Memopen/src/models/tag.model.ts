4c1284ffb7df6ca1c6e753f965cf517c86c54a0a
Seamew <yutsuzetime@gmail.com>
2025-05-12 22:55:11 +0700

    create post model kub

--- backend-Memopen/src/models/tag.model.ts
+++ backend-Memopen/src/models/tag.model.ts
@@ -1,24 +1,33 @@
 import { db } from "../index.ts";
 
 const getTag = async() => {
-    const tag = await db.tag.findMany();
-    return tag
+    const tag = await db.tag.findMany({
+        include: {
+            posts: true,
+        }
+    })
 };
 
-const addTag = async() => {
-    const newTag = await db
-    return newTag
-}
+const addTag = async (title: string, color: string) => {
+    return await db.tag.create({
+      data: { title, color,
+      },
+    });
+  };
 
-const deleteTag = async() => {
-    const deletedTag = await db
-    return deletedTag
-}
+  const deleteTag = async (tagId: number) => {
+    return await db.tag.delete({
+      where: { id: tagId },
+    });
+  };
 
-const editTag = async () => {
-    const editedTag = await db
-    return editedTag;
-}
-
+  const editTag = async (tagId: number, title: string, color: string) => {
+    return await db.tag.update({
+      where: { id: tagId },
+      data: {
+        title, color,
+      },
+    });
+  };
 
 export { getTag, addTag, deleteTag, editTag}
